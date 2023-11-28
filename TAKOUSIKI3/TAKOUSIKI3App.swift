d755298ad2af3790dd2b0affb7e90ddb59267332
kitachris <kitachris19@gmail.com>
2023-11-28 23:08:08 +0900

    Initial Commit

---
+++ TAKOUSIKI3/TAKOUSIKI3App.swift
@@ -0,0 +1,17 @@
+//
+//  TAKOUSIKI3App.swift
+//  TAKOUSIKI3
+//
+//  Created by KOJI KITAJIMA on 2023/11/28.
+//
+
+import SwiftUI
+
+@main
+struct TAKOUSIKI3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
